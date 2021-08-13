6d28e9ac35bb9b3f674c8273a562148912c42e55
Anny Pereira <isabelle.pereira04@gmail.com>
2021-08-13 08:13:48 -0300

    Exercício e Resolução

---
+++ Exercícios/Resolução_Exercícios/SCRIPTS/Livraria_01_DDL.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE LIVRARIA;
+GO
+
+USE LIVRARIA;
+GO
+
+CREATE TABLE Livraria (
+idLivraria SMALLINT PRIMARY KEY IDENTITY (1,1),
+nomeLivraria VARCHAR(20)
+);
+GO
+
+CREATE TABLE Categoria(
+idCategoria SMALLINT PRIMARY KEY IDENTITY(1,1),
+nomeCategoria  VARCHAR(20)
+);
+GO
+
+
+CREATE TABLE Livro (
+idLivro SMALLINT PRIMARY KEY IDENTITY(1,1),
+idLivraria SMALLINT FOREIGN KEY REFERENCES Livraria(idLivraria),
+idCategoria SMALLINT FOREIGN KEY REFERENCES Categoria(idCategoria),
+nomeLivro VARCHAR (45) NOT NULL,
+autor VARCHAR (50),
+precoLivro SMALLINT
+);
+GO
